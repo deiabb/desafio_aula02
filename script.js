1c825b0de1019387bad009c45acffe68de4c6ee7
Deborah B Barra <deborahbbarra@gmail.com>
2024-05-10 13:45:15 -0300

    Criadas variaveis para nome e sobrenome e ano atual

---
+++ script.js
@@ -0,0 +1,7 @@
+var nome = prompt("Digite seu nome");
+var sobrenome = prompt("Digite seu sobrenome");
+alert(`Olá padawan ${nome} ${sobrenome}`); 
+var anoLancamento = 1977;
+var numeroDigitado = prompt("Digite o ano atual");
+var quantosAnos = parseInt(numeroDigitado) - anoLancamento;
+alert(`Faz ${quantosAnos} anos que "Uma Nova Esperança" foi lançado nos cinemas.`)
